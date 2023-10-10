6a6d20fc60bc8d633c768ca578aff3735ddefcfb
Wang Tianshuo <15130709273@163.com>
2023-10-10 17:50:28 +0800

    change hello to hey.

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main(int argc, char **argv)
 {
  char first[255], last[255];
  askname(first, last);
-printf("Hello, %s %s!\n", first, last);
+printf("Hey, %s %s!\n", first, last);
  return 0;
 }
 /* askname.c */
